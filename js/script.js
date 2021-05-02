2741a793971bf9b20e4d00343347167544d4ff67
Alex <alexandrasauri19@gmail.com>
2021-05-02 19:32:24 +0400

    added transition to burger menu and added tab icon

--- js/script.js
+++ js/script.js
@@ -3,7 +3,12 @@ let navigation = document.querySelector('nav')
 burgerMenu.addEventListener('click', function(){
    if(navigation.classList.contains('desktop-only')){
        navigation.classList.remove('desktop-only')
+       burgerMenu.style.transform = 'rotate(90deg)'
+       burgerMenu.style.transitionDuration= '500ms'
+
    } else{
        navigation.classList.add('desktop-only')
+       burgerMenu.style.transform = 'rotate(0deg)'
+
    }
 })
